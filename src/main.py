4d3cdf58a08ae9818dbbace2e0c7936b02657b5a
Milan_Prasad <126258981+Sanmil4444@users.noreply.github.com>
2026-04-15 15:58:39 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,16 @@
+from data_loader import load_data
+from processor import clean_data, calculate_kpis
 
+def run_pipeline():
+    df = load_data("../data/sample_data.csv")
+    
+    if df is None:
+        return
+    
+    df = clean_data(df)
+    kpis = calculate_kpis(df)
+
+    print(kpis)
+
+if __name__ == "__main__":
+    run_pipeline()
